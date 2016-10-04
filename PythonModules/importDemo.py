f959b5aa9002e5a792ddfdec529c8a0ae4bdfbb0
John Fay <john.fay@duke.edu>
2016-10-04 14:23:09 -0400

    Updates the importDemo.py script

--- PythonModules/importDemo.py
+++ PythonModules/importDemo.py
@@ -1,3 +1,8 @@
+#importDemo.py
+#
+#Description:
+# A simple examination of importing modules and how to access them once imported
+
 #Use the dir() function to list all loaded modules and variables
 print dir()
 
@@ -6,9 +11,9 @@ from os import path
 dir() # See what was imported
 print path.join("C:\\temp","myFile.txt")
 
-# Import a module, giving it a name you choose
-import win32com.client as win32c
-print dir(win32c)
+# Import a module [here the numpy module], giving it a name you choose [here 'np']
+import numpy as np
+print dir(np)
 
 # Import all subcomponents of a module, giving access to them 
 # without needing to preface a function with the module's name
@@ -17,5 +22,5 @@ print dir()
 print sqrt(25)
 
 # Remove reference to a specific module from your script/session
-del win32c
-print dir(win32c) # <-- this will now give you an error
+del np
+print dir(np) # <-- this will now give you an error
